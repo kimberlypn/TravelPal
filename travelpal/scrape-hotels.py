c5ea06d6f82c59e704418a4eba656f49177e8acd
Long <lin.lo@husky.neu.edu>
2018-04-13 14:05:40 -0400

    update scraper to include headers, parse and load results into a dictionary

--- travelpal/scrape-hotels.py
+++ travelpal/scrape-hotels.py
@@ -3,28 +3,39 @@ from bs4 import BeautifulSoup
 import html5lib
 
 
-url = 'https://www.booking.com/searchresults.html?label=gen173nr-1FCAEoggJCAlhYSDNYBHIFdXNfbWGIAQGYATHCAQN4MTHIAQzYAQHoAQH4AQKSAgF5qAID&sid=129654e8254415d360a1922d47c7ccf7&sb=1&src=searchresults&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Fsearchresults.html%3Flabel%3Dgen173nr-1FCAEoggJCAlhYSDNYBHIFdXNfbWGIAQGYATHCAQN4MTHIAQzYAQHoAQH4AQKSAgF5qAID%3Bsid%3D129654e8254415d360a1922d47c7ccf7%3Bcheckin_monthday%3D20%3Bcheckin_year%3D2018%3Bcheckout_month%3D4%3Bcheckout_monthday%3D21%3Bcheckout_year%3D2018%3Bclass_interval%3D1%3Bdest_id%3D-2637882%3Bdest_type%3Dcity%3Bfrom_sf%3D1%3Bgenius_rate%3D1%3Bgroup_adults%3D2%3Bgroup_children%3D0%3Blabel_click%3Dundef%3Bno_rooms%3D1%3Boffset%3D0%3Braw_dest_type%3Dcity%3Broom1%3DA%252CA%3Bsb_price_type%3Dtotal%3Bsrc%3Dindex%3Bsrc_elem%3Dsb%3Bss%3DTaipei%3Bssb%3Dempty%3Bssne%3DTaipei%3Bssne_untouched%3DTaipei%26%3B&ss=Taipei&ssne=Taipei&ssne_untouched=Taipei&city=-2637882&checkin_month=4&checkin_monthday=19&checkin_year=2018&checkout_month=4&checkout_monthday=20&checkout_year=2018&group_adults=2&group_children=0&no_rooms=1&from_sf=1&user_changed_date=1'
+url = 'https://www.booking.com/searchresults.html?label=gen173nr-1FCAEoggJCAlhYSDNYBHIFdXNfbWGIAQGYATHCAQN4MTHIAQzYAQHoAQH4AQKSAgF5qAID;sid=00e247fa4231bdeb9d723c9afc1f8aa7;checkin_month=4&checkin_monthday=20&checkin_year=2018&checkout_month=4&checkout_monthday=21&checkout_year=2018&class_interval=1&dest_id=-2637882&dest_type=city&dtdisc=0&from_sf=1&genius_rate=1&group_adults=2&group_children=0&inac=0&index_postcard=0&label_click=undef&no_rooms=1&offset=0&postcard=0&raw_dest_type=city&room1=A%2CA&sb_price_type=total&src=index&src_elem=sb&ss=Taipei&ss_all=0&ssb=empty&sshis=0&ssne=Taipei&ssne_untouched=Taipei&'
 
-response = get(url)
+head = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36"}
+
+response = get(url, headers=head)
 
 print(response.status_code)
-#print(response.text)
 
-f = open('scraped-results.txt','w')
-f.write(response.text)
-f.close()
+#f = open('scraped-results.txt','w')
+#f.write(response.text)
+#f.close()
 
 # parse response into bs object
 html_soup = BeautifulSoup(response.text, "html5lib")
 
-hotel_names = html_soup.find_all(class_='sr-hotel__name')
-print(len(hotel_names))
+listing = html_soup.find_all(class_='sr_item')
+hotel_list = {}
 
-prices = html_soup.find_all(class_='availprice')
-print(len(prices))
+for hotel in listing:
+  name = hotel.select_one("span.sr-hotel__name")
+  price = hotel.select_one(".price")
 
-for name in hotel_names: 
+  if name and price:
+    name = name.text.strip()
+    price = price.text.strip()
+    price = price[price.find('$') + 1:] 
+    hotel_list[name] = int(price)
+ 
+
+for name, price in hotel_list.items():
   print(name)
-
-for price in prices:
   print(price)
+  print("------")
+
+
+print("Retrieved information of " + str(len(hotel_list)) + " hotels")
