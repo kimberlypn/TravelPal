670fe847d8ab286017b092f7a831701bbeba6448
Long <lin.lo@husky.neu.edu>
2018-04-13 14:29:22 -0400

    add upgrade pip command to install dependecy script

--- travelpal/install_deps.sh
+++ travelpal/install_deps.sh
@@ -3,7 +3,8 @@ sudo apt-get update  # To get the latest package lists
 echo Current Python3 version is # Python should come preloaded with ubuntu
 python3 -V
 
-sudo apt-get install python3-pip # install pip for package management
+sudo apt-get install python3-pip3 # install pip for package management
+pip install --upgrade pip
 pip install requests # for fetching requests
 pip install beautifulsoup4 # for parsing responses
 
