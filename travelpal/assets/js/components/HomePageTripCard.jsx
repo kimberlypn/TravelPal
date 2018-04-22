19a8538f1a063993bd08ed3bb8a3edc9314b8c40
Longsheng Lin <lin.lo@husky.neu.edu>
2018-04-21 22:32:54 -0400

    Home page trip name (#40)
    * add traveler name to homepage trips, add proptype checks
    
    * remove console logs

--- travelpal/assets/js/components/HomePageTripCard.jsx
+++ travelpal/assets/js/components/HomePageTripCard.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import api from '../api';
 
 // Renders the details of a hotel
-export default function HomePageTripCard({destination, startDate, endDate, summary, hotel}) {
+export default function HomePageTripCard({traveler, destination, startDate, endDate, summary, hotel}) {
   const hotelName = hotel ? hotel.name : ""
   const hotelImage = hotel ? hotel.image_src : ""
 
@@ -17,6 +17,7 @@ export default function HomePageTripCard({destination, startDate, endDate, summa
       <h5 className="card-header">{toDateReadable(startDate)}  -  {toDateReadable(endDate)}</h5>
       <div className="card-body row">
         <div className="col-9">
+          <p className="card-tetx">Traveler: {traveler}</p>
           <p className="card-tetx">Destination: {destination}</p>
           <p className="card-text">Summary: {summary}</p>
           <p className="card-text">Staying at: {hotelName}</p>
@@ -30,4 +31,9 @@ export default function HomePageTripCard({destination, startDate, endDate, summa
 };
 
 HomePageTripCard.propTypes = {
+  traveler: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+  startDate: PropTypes.string.isRequired,
+  endDate: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
 };
